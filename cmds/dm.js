a41516910556401359072e73cba5073e5fb25886
RomanSpector <57856647+RomanSpector@users.noreply.github.com>
2020-10-01 15:13:29 +0300

    Create dm.js

---
+++ cmds/dm.js
@@ -0,0 +1,8 @@
+const Discord = module.require("discord.js");
+const fs = require("fs");
+module.exports.run = async (bot,message,args) => {
+    message.channel.send('Nameplate Diminishing: https://gist.github.com/Bunny67/c4fa63341851240ddbf113ec907fe8ab');
+};
+module.exports.help = {
+    name: "dm"
+};
