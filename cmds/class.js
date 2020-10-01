3d091619a99d5c65d129fbb19ded6a1090d0f06a
RomanSpector <57856647+RomanSpector@users.noreply.github.com>
2020-10-01 17:30:42 +0300

    Add files via upload

---
+++ cmds/class.js
@@ -0,0 +1,8 @@
+const Discord = module.require("discord.js");
+const fs = require("fs");
+module.exports.run = async (bot,message,args) => {
+    message.channel.send('\n!dru');
+};
+module.exports.help = {
+    name: "class"
+};
---
+++ cmds/dru.js
@@ -0,0 +1,8 @@
+const Discord = module.require("discord.js");
+const fs = require("fs");
+module.exports.run = async (bot,message,args) => {
+    message.channel.send('https://discordapp.com/channels/750381752752668682/750384812078137344/760547398790545499');
+};
+module.exports.help = {
+    name: "dru"
+};
