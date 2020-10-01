4ae10d85cbbaf90ac72b4ff06a24ec1b1d7e2435
RomanSpector <57856647+RomanSpector@users.noreply.github.com>
2020-10-01 15:07:22 +0300

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -1,7 +1,7 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
-    message.channel.send('\n!WA\n!class\n!DM\n!slot');
+    message.channel.send('\n!wa\n!class\n!DM\n!slot');
 };
 module.exports.help = {
     name: "help"
