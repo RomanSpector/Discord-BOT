d591a07138f2a9ddb698794b114d673802a641de
RomanSpector <57856647+RomanSpector@users.noreply.github.com>
2020-10-03 10:49:43 +0300

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -1,7 +1,7 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
 module.exports.run = async (bot,message,args) => {
-    message.channel.send('\n!wa - последняя версия аддона\n!class - викауры по классам\n!dm - диминишинг на неймплейтах\n!slot - номера слотов для экипировки');
+    message.channel.send('\n!mylvl - уровень активности на канале\n!wa - последняя версия аддона\n!class - викауры по классам\n!dm - диминишинг на неймплейтах\n!slot - номера слотов для экипировки');
 };
 module.exports.help = {
     name: "help"
