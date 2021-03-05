90d707c7c4f790cef0cfe8baa19492de046c9a72
Roman Spector <ismailroma@yandex.ru>
2021-03-05 15:30:18 +0300

    test

--- bot.js
+++ bot.js
@@ -5,8 +5,8 @@ const cmd = require("./comms.js");
 let config = require("./botconfig.json"); 
 let prefix = config.prefix;
 
-const channelID = "761857830923665418";
-const ReactMsg= "817331548256731167";
+const channelID = "768549340460875806";
+const ReactMsg= "817373334831497257";
 
 bot.on("ready",  () => { 
     console.log(`Запустился бот ${bot.user.username}`);
